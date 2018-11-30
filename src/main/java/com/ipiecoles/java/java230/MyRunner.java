1e8fa8cc486b38a12a0e30ced29acdb4b139f5e9
charaf69780 <charaf.besbas@ipilyon.net>
2018-11-30 10:49:02 +0100

    ex 3

--- src/main/java/com/ipiecoles/java/java230/MyRunner.java
+++ src/main/java/com/ipiecoles/java/java230/MyRunner.java
@@ -1,16 +1,35 @@
 package com.ipiecoles.java.java230;
 
+import com.ipiecoles.java.java230.model.Employe;
+import org.joda.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import com.ipiecoles.java.java230.repository.EmployeRepository;
 
 @Component
 public class MyRunner implements CommandLineRunner {
 
-    @Override
-    public void run(String... strings) throws Exception {
 
+        @Autowired
+        private EmployeRepository employeRepository;
 
-    }
+        @Override
+        public void run(String... strings) throws Exception {
+
+            System.out.println("coucou");
+            Employe e = employeRepository.findOne(4L);
+
+            //Delete
+            //employeRepository.delete(2521L);
+            
+            //Create
+            Employe employe = new Employe("Doe","John","X6666", LocalDate.now(),);
+            System.out.println(employe.getId());//Null
+            employe = employeRepository.save(employe);
+            System.out.println(employe.getId());
+        }
+
 
     public static void print(Object t) {
         System.out.println(t);
